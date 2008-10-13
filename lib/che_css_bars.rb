8064b820f1724f1c9f3b9708e6072c8ed50a0897
cheef <cheef.che@gmail.com>
2008-10-13 18:03:07 +0400

    fix stylesheet including

--- lib/che_css_bars.rb
+++ lib/che_css_bars.rb
@@ -1,15 +1,15 @@
 module Che
   module CssBars
-    def css_bars_include_tag(content_to)
+    def css_bars_include_tag(content_to = "")
       return nil if @css_bars_included
+      @css_bars_included = true
       content_to.blank? ? 
         stylesheet_link_tag('css_bars/css_bars', :media => 'all') :      
           content_for(content_to){stylesheet_link_tag 'css_bars/css_bars', :media => 'all'}
-      @css_bars_included = true
     end
 
     def horizontal_bar(data, css_to = "")
-      css_bars_include_tag css_to
+      css_bars_include_tag css_to unless css_to.blank?
       CssBarRenderer.new(@template, data).render
     end
   end
